a0107c613eff96c86b0014c8bed7a20abc2870a7
GaouerRaouf <93593515+GaouerRaouf@users.noreply.github.com>
2021-12-23 18:57:16 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,85 +1,116 @@
-import { Col, Row, FormControl, InputGroup, Button, Container } from 'react-bootstrap';
-import aiesecLogo from '../Images/aiesecLogo.svg';
-import facebook from '../Images/facebook.svg';
-import linkedin from '../Images/linkedin.svg';
-import whatsapp from '../Images/whatsapp.svg';
-import youtube from '../Images/youtube.svg';
-import twitter from '../Images/twitter.svg';
-import ScrollUp from '../Images/ScrollUp.svg';
-import '../Styles/Footer.css';
+import {
+  Col,
+  Row,
+  FormControl,
+  InputGroup,
+  Button,
+  Container,
+} from "react-bootstrap";
+import aiesecLogo from "../Images/aiesecLogo.svg";
+import facebook from "../Images/facebook.svg";
+import linkedin from "../Images/linkedin.svg";
+import whatsapp from "../Images/whatsapp.svg";
+import youtube from "../Images/youtube.svg";
+import twitter from "../Images/twitter.svg";
+import ScrollUp from "../Images/ScrollUp.svg";
+import {HashLink as Link} from "react-router-hash-link"
+import "../Styles/Footer.css";
 
-const Footer = () =>{
-    return (
-        <Container fluid style={{padding:"5rem 5rem 0 5rem", textAlign: 'center'}} >
-        <img src={aiesecLogo} alt='logobig' className='py-5' width="600px"/>
+const Footer = () => {
+  return (
+    <Container
+      fluid
+      style={{ padding: "5rem 5rem 2rem 5rem", textAlign: "center" }}
+    >
+      <img src={aiesecLogo} alt="logobig" className="py-5" width="600px" />
 
-            <Row className="Footer-row">
-                <Col className="pt-5 align">
-                <h4 className='headers'>About</h4>
-                <p>About us</p>
-                <p>Annual Report</p>
+      <Row className="Footer-row">
+        <Col className="pt-5 align">
+          <h4 className="headers">About</h4>
+          <p>About us</p>
+          <p>Annual Report</p>
 
-                <h4 className='headers'>Organizations</h4>
-                <p className="">Partner with us</p>
-                <p>Pricing</p>
-                </Col>
-                
-                <Col className="pt-5 align">
-                <h4 className='headers'>Youth</h4>
-                <p>Youth 4 Global Goals</p>
-                <p>Youth Speak</p>
-                <div className='social'>
+          <h4 className="headers">Organizations</h4>
+          <p className="">Partner with us</p>
+          <p>Pricing</p>
+        </Col>
 
-                             <a style={{margin: '0.5rem'}} href="https://facebook.com">
-                                     <img width="40px" src={facebook} alt="facebook" />
-                             </a>
-                             <a style={{margin: '0.5rem'}} href="https://linkedin.com">
-                                     <img width="40px" src={linkedin} alt="linkedin" />
-                             </a>
-                             <a style={{margin: '0.5rem'}} href="https://youtube.com">
-                                     <img width="40px" src={youtube} alt="youtube" />
-                             </a>
-                             <a style={{margin: '0.5rem'}} href="https://whatsapp.com">
-                                     <img width="40px" src={whatsapp} alt="whatsapp" />
-                             </a>
-                             <a style={{margin: '0.5rem'}} href="https://twitter.com">
-                                     <img width="40px" src={twitter} alt="twitter" />
-                             </a>  
-                    <p className='copyright'>
-                     Powered by  AIESEC <br/>
-                 ©   1948-2021  AIESEC
-                    </p>
+        <Col className="pt-5 align">
+          <h4 className="headers">Youth</h4>
+          <p>Youth 4 Global Goals</p>
+          <p>Youth Speak</p>
+          <div className="social">
+            <a style={{ margin: "0.5rem" }} href="https://facebook.com">
+              <img width="40px" src={facebook} alt="facebook" />
+            </a>
+            <a style={{ margin: "0.5rem" }} href="https://linkedin.com">
+              <img width="40px" src={linkedin} alt="linkedin" />
+            </a>
+            <a style={{ margin: "0.5rem" }} href="https://youtube.com">
+              <img width="40px" src={youtube} alt="youtube" />
+            </a>
+            <a style={{ margin: "0.5rem" }} href="https://whatsapp.com">
+              <img width="40px" src={whatsapp} alt="whatsapp" />
+            </a>
+            <a style={{ margin: "0.5rem" }} href="https://twitter.com">
+              <img width="40px" src={twitter} alt="twitter" />
+            </a>
+            <p className="copyright">
+              Powered by AIESEC <br />© 1948-2021 AIESEC
+            </p>
+          </div>
+        </Col>
 
-                    </div>
-                </Col>
-                
-                <Col className="pt-5 align">
-                <h4 className='headers'>NEWS LATTER</h4>
-                <p>stay update with last offers and news</p>
-                
-                <InputGroup style={{paddingLeft: '0', paddingRight: '7rem', marginBottom: '2rem', minHeight: "60px"}}>
+        <Col className="pt-5 align">
+          <h4 className="headers">NEWS LATTER</h4>
+          <p>stay update with last offers and news</p>
 
-    <FormControl 
-      type="email"
-      placeholder="   Enter your email"
-      aria-label="email"
-      aria-describedby="basic-addon2"
-      style={{borderStyle: 'solid',borderRadius: '10px' ,borderWidth: '3px', borderColor: '#477DEE'}}
-    />
-    <Button style={{borderStyle: 'solid',borderRadius: '10px' ,borderWidth: '3px', position: "relative", left:"-15px",BackgroundColor:"#477DEE", paddingLeft: "2rem", paddingRight: "2rem"}}  size="lg" id="button-addon2">
-    Send 
-    </Button>
-  </InputGroup>
+          <InputGroup
+            style={{
+              paddingLeft: "0",
+              paddingRight: "7rem",
+              marginBottom: "2rem",
+              minHeight: "60px",
+            }}
+          >
+            <FormControl
+              type="email"
+              placeholder="   Enter your email"
+              aria-label="email"
+              aria-describedby="basic-addon2"
+              style={{
+                borderStyle: "solid",
+                borderRadius: "10px",
+                borderWidth: "3px",
+                borderColor: "#477DEE",
+              }}
+            />
+            <Button
+              style={{
+                borderStyle: "solid",
+                borderRadius: "10px",
+                borderWidth: "3px",
+                position: "relative",
+                left: "-15px",
+                BackgroundColor: "#477DEE",
+                paddingLeft: "2rem",
+                paddingRight: "2rem",
+              }}
+              size="lg"
+              id="button-addon2"
+            >
+              Send
+            </Button>
+          </InputGroup>
 
+          <h4 className="headers">Membership</h4>
 
-                <h4 className='headers'>Membership</h4>
-
-                <p>Join Us</p>
-                <p>Find your country!</p>
-                
-                </Col>
-            </Row>
-        </Container>   
-    );
+          <p>Join Us</p>
+          <p>Find your country!</p>
+         <button className='btn back-to-top-button'><Link smooth to='#top'><img src={ScrollUp}/></Link></button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 export default Footer;
